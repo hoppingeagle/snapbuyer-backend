60416101bcf44b771c9a3cc6a68b74de51ce44a9
Maciej Mikulski <maciej.mikulski@sparklab.pl>
2015-03-13 23:03:46 +0100

    Changed placeholder urls to image urls

--- app/Http/Controllers/Allegro/AllegroController.php
+++ app/Http/Controllers/Allegro/AllegroController.php
@@ -1,6 +1,7 @@
 <?php namespace Snapbuyer\Http\Controllers\Allegro;
 
 use Snapbuyer\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 
 /**
  * Created by PhpStorm.
@@ -17,15 +18,22 @@ class AllegroController extends Controller
         return response()->json($itemsList);
     }
 
+
+
     private function randomItems($number)
     {
         $items = [];
 
         for ($i = 1; $i <= $number; $i++) {
             $id = rand(1000000000,9999999999);
+            $url = [
+                'http://img16.allegroimg.pl/photos/oryginal/50/93/09/85/5093098540',
+                'http://img09.allegroimg.pl/photos/oryginal/50/98/00/72/5098007215',
+                'http://img03.allegroimg.pl/photos/oryginal/51/63/32/64/5163326440'
+            ];
             $item = [
                 'id' => $id,
-                'url'=> 'http://allegro.pl/ShowItem2.php?item='.$id
+                'url'=> $url[rand(0,2)]
             ];
             $items[] = $item;
         }
